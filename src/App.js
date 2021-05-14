cb4e9c87ebc88d970fd612f1b5431bec44ae48af
Marina Monsivais <minamonsi@me.com>
2021-05-13 21:15:33 -0500

    modified some files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import './App.css';
 import Home from './components/Home'
 import Food from './components/Food'
 import Drinks from './components/Drinks'
 
+
 const App = () => {
   return (
     <BrowserRouter>
-    <div>
-    <h1>Lambda Eats</h1>
+    <div class="lambdaApp">
+      <header>
+      <h1>Lambda Eats</h1>
+      </header>
+
     <Link to='/' style={{padding: 20}}>Home</Link>
     <Link to='/food' style={{padding: 20}}>Food</Link> 
     <Link to='/drinks' style={{padding: 20}}>Drinks</Link>
