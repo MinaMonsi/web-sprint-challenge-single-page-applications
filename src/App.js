18847dd326ec8726842e3651176b1cdf0d6c0315
Marina Monsivais <minamonsi@me.com>
2022-04-04 23:26:52 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from "react";
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
-import Home from './components/Home'
-import Food from './components/Food'
-import Drinks from './components/Drinks'
+import Header from './components/Header';
+import Home from './components/Home';
+import Food from './components/Food';
+import Drinks from './components/Drinks';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+
     <div class="lambdaApp">
-      <header>
-      <h1>Lambda Eats</h1>
-      </header>
-
-    <Link to='/' style={{padding: 20}}>Home</Link>
-    <Link to='/food' style={{padding: 20}}>Food</Link> 
-    <Link to='/drinks' style={{padding: 20}}>Drinks</Link>
-
-    <Switch>
-    <Route exact path='/' component={Home}/>
-    <Route path='/food' component={Food}/> 
-    <Route path='/drinks' component={Drinks}/>
-    </Switch>
+      <Header /> 
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/food' component={Food}/> 
+        <Route path='/drinks' component={Drinks}/>
+      </Switch>
     </div>
+
     </BrowserRouter>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../App.css"
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const history = useHistory();
+    
     return(
         <div>
-            <button className="button" onClick={(evt) => history.push(`/`)}>Home</button>
-            <button className="button" onClick={(evt) => history.push(`/help`)}>Help</button>
+            <header>
+                <h1>Lambda Eats</h1>
+            </header>
+
+            <Link to='/' style={{padding: 20}}>Home</Link>
+            <Link to='/food' style={{padding: 20}}>Food</Link> 
+            <Link to='/drinks' style={{padding: 20}}>Drinks</Link>
 
         </div>
     )
